6b26da6ba5f5f59ad8328391d23e912b8aee6e24
jibon2016 <sdjibon2016@gmail.com>
2024-08-11 21:44:52 +0600

    Inital Commit

---
+++ database/migrations/2024_08_11_132455_create_properties_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('location');
+            $table->decimal('value_min');
+            $table->decimal('value_max');
+            $table->string('beds');
+            $table->string('bath');
+            $table->string('balcony');
+            $table->string('decoration');
+            $table->string('carpet_area')->nullable();
+            $table->string('floor')->nullable();
+            $table->string('transaction_type');
+            $table->integer('lift');
+            $table->integer('facing');
+            $table->string('additional_rooms');
+            $table->string('age_of_construction');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('properties');
+    }
+};
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    phpVersion: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <div class="">
+
+    </div>
+</template>
